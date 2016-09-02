44d7b3d5b594f941468f524969685ef7d3184cd8
Jai <govindani@gmail.com>
2016-09-02 13:40:40 +0700

    Update list-distribution-groups.ps1

--- list-distribution-groups.ps1
+++ list-distribution-groups.ps1
@@ -1,4 +1,7 @@
-foreach ($group in Get-DistributionGroup) 
+	foreach ($group in Get-DistributionGroup) 
 	{ 
-		get-distributiongroupmember $group | sort displayname | ft @{expression={$_.displayname};Label=”$group”} | Out-File c:\temp\DistributionListMembers.txt -append
+		get-distributiongroupmember $group | 
+		sort displayname | 
+		ft @{expression={$_.displayname};Label="$group - " + $group.primarysmtpaddress} | 
+		Out-File c:\IT\DistributionListMembers.txt -append
 	}
